4d2d977c1fe439715bbb216ad84287105797a379
fredie7385 <mukii7385@gmail.com>
2023-12-04 18:07:43 +0300

    updated

--- src/Forms/Contact.jsx
+++ src/Forms/Contact.jsx
@@ -41,7 +41,7 @@ function Contact() {
          {/* google maps */}      
          <div className="google-map">
          <iframe title='map'src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d257032.32727352905!2d-0.4474951988852126!3d51.5360261225924!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2ske!4v1701698249132!5m2!1sen!2ske"
-         allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+         allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
          </div>
          <Container fluid>
             <div className="contact-info">
